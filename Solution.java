5a6909bd72a152dfeb3ddc6c507ca850c067e291
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-08-14 21:02:37 +0530

    commit

--- Solution.java
+++ Solution.java
@@ -627,7 +627,45 @@ class Solution {
   }
 }
 ****************************************************************************
-Question_No 12) 
+Question_No 12) Find Kth Smallest Pair Distance
+The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+Example 1:
+Input: nums = [1,3,1], k = 1
+Output: 0
+Explanation: Here are all the pairs:
+(1,3) -> 2
+(1,1) -> 0
+(3,1) -> 2
+Then the 1st smallest distance pair is (1,1), and its distance is 0.
+Example 2:
+Input: nums = [1,1,1], k = 2
+Output: 0
+Example 3:
+Input: nums = [1,6,1], k = 3
+Output: 5
+
+Solutions:
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int left = 0, right = nums[nums.length - 1];
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int j = 0;
+            int count = 0;
+            for(int i = 0; i < len; i++){
+                while(j < len && nums[j] - nums[i] <= mid) ++j;
+                count += j - i - 1;
+            }
+            if(count >= k) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
 
  
         
