08ef613b546c4e39c8c3dbc594d24e11213d6885
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-08-16 23:04:24 +0530

    Commit

--- Solution.java
+++ Solution.java
@@ -720,11 +720,42 @@ class Solution {
             }
         }
         return true;
-        
-        
+          }
+}
+ ********************************************************************
+ Question_No 14)
+ Maximum Distance in Array
+ You are given m arrays, where each array is sorted in ascending order.
+You can pick up two integers from two different arrays (each array picks one) and calculate the distance.
+ We define the distance between two integers a and b to be their absolute difference |a - b|.
+Return the maximum distance.
+Example 1:
+Input: arrays = [[1,2,3],[4,5],[1,2,3]]
+Output: 4
+Explanation: One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+Example 2:
 
+Input: arrays = [[1],[1]]
+Output: 0
+
+Solution:
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        int ans = 0;
+        int mi = arrays.get(0).get(0);
+        int mx = arrays.get(0).get(arrays.get(0).size() - 1);
+        for (int i = 1; i < arrays.size(); ++i) {
+            var arr = arrays.get(i);
+            int a = Math.abs(arr.get(0) - mx);
+            int b = Math.abs(arr.get(arr.size() - 1) - mi);
+            ans = Math.max(ans, Math.max(a, b));
+            mi = Math.min(mi, arr.get(0));
+            mx = Math.max(mx, arr.get(arr.size() - 1));
+        }
+        return ans;
     }
 }
+ 
 
  
         
